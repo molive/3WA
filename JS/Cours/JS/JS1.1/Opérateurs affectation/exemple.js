3d8c196a66293c96472915e8fbee45bf1dd55ab3
Molive <the.molive@gmail.com>
2016-10-29 14:07:22 +0200

    Ajout DM sur les boucles

---
+++ JS/Cours/JS/JS1.1/Opérateurs affectation/exemple.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Expressions_et_Op%C3%A9rateurs#Opérateurs_d'affectation
+
+// Opérateur =
+
+var test = 1;
+var test2 = "chaine de caractères";
+
+//opérateur +=
+var test = "hello";
+test += "world"; // test = hello world
+
+var test2 = 1;
+test2 += 4; //test2 = 5
+
+// -=
+var test3 = 5;
+test3 -= 2; //test3 = 3
+
+// *=
+var test3 = 5;
+test3 *= 2; //test3 = 10
