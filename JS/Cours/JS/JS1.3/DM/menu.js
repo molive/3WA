dedaf1517a800270971ed934ddadef83f2ace111
Molive <the.molive@gmail.com>
2016-10-30 13:27:14 +0100

    Intégration jQuery TP boucles

--- JS/Cours/JS/JS1.3/DM/menu.js
+++ JS/Cours/JS/JS1.3/DM/menu.js
@@ -1,16 +1,16 @@
 // PARTIE 1:
 "use strict";  // utilisation du mode strict (https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Strict_mode)
 
-var menu = [{ category : "Animaux" ,
-				 items : ["Garde d'animaux", "Promenade d'animaux","Soin d'animaux"]
-			},
-			{ category : "Automobile et 2 roues" ,
-		         items : ["Carrosserie et peinture auto", "Covoiturage","Dépannage et diagnostic auto","Mécanicien et garagiste"]
-			},
-			{ category : "Informatique et web" ,
-			     items : ["Dépannage et réparation informatique", "Montage photo et vidéo", "Webmastering"]	      
-			}];
-
+var menu = [{
+    category: "Animaux",
+    items: ["Garde d'animaux", "Promenade d'animaux", "Soin d'animaux"]
+}, {
+    category: "Automobile et 2 roues",
+    items: ["Carrosserie et peinture auto", "Covoiturage", "Dépannage et diagnostic auto", "Mécanicien et garagiste"]
+}, {
+    category: "Informatique et web",
+    items: ["Dépannage et réparation informatique", "Montage photo et vidéo", "Webmastering"]
+}];
 
 /* Générez à partir de la variable menu le code html ci-dessous:
 
@@ -49,21 +49,19 @@ var menu = [{ category : "Animaux" ,
 
 // PARTIE 2:
 
-menu = [{ category : "Animaux" ,
-				 items : ["Garde d'animaux", "Promenade d'animaux","Soin d'animaux"]
-			},
-			{ category : "Automobile et 2 roues" ,
-		         items : ["Carrosserie et peinture auto", "Covoiturage","Dépannage et diagnostic auto","Mécanicien et garagiste"]
-			},
-			{ category : "Informatique et web" ,
-			     items : ["Dépannage et réparation informatique", 
-					      "Montage photo et vidéo", 
-					      { category : "Webmastering" ,
-						    items : ["Intégrateur web", "Développement web","Référencement","Infographisme"]
-					      }]
-			}];
-
-
+menu = [{
+    category: "Animaux",
+    items: ["Garde d'animaux", "Promenade d'animaux", "Soin d'animaux"]
+}, {
+    category: "Automobile et 2 roues",
+    items: ["Carrosserie et peinture auto", "Covoiturage", "Dépannage et diagnostic auto", "Mécanicien et garagiste"]
+}, {
+    category: "Informatique et web",
+    items: ["Dépannage et réparation informatique", "Montage photo et vidéo", {
+        category: "Webmastering",
+        items: ["Intégrateur web", "Développement web", "Référencement", "Infographisme"]
+    }]
+}];
 
 /* Générez à partir de la variable menu le code html ci-dessous:
 
